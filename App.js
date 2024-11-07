f316af6fae497779c9b3680a12df16e50128f14e
algocomtedio <caio.vieira58@etec.sp.gov.br>
2024-11-07 23:31:22 +0000

    criacao do projeto e caixa de texto

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aplicativo-calculo-IMC</Text>
+      <TextInput placeholder="peso" keyboardType="numeric" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
